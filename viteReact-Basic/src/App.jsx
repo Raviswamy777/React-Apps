3989553a66b33d675c9c37979d10d9d1993907db
raviswamy777 <raviswamy@i.camp>
2024-07-30 19:48:14 +0530

    bg Changer

--- viteReact-Basic/src/App.jsx
+++ viteReact-Basic/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState();
+  function onsubmit(userColor){
+    setColor(userColor)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-screen duration-1000 bg-white text-blue"  style={{backgroundColor:color}}>
+        <center> <h1 className="text-9xl font-serif">Hello</h1></center>
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2" >
+          <div className="flex flex-wrap justify-center gap-3 shadow-2xl bg-yellow-400 px-3 py-2 rounded-3xl">
+            <button className="outline-none px-4 py-1 rounded-full bg-white shadow-lg text-black" onClick={()=>onsubmit('white')}>Normal</button>
+            <button className="outline-none px-4 py-1 rounded-full bg-white shadow-lg text-black" onClick={()=>onsubmit('lightBlue')}>Light Blue</button>
+            <button className="outline-none px-4 py-1 rounded-full bg-white shadow-lg text-black" onClick={()=>onsubmit('cyan')}>Cyan</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
