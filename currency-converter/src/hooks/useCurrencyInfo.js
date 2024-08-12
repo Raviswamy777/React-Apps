df80508e9d8598c79912db197bf15297be07eab0
raviswamy777 <raviswamy@i.camp>
2024-08-12 23:05:19 +0530

    custom hook for currency feth

--- currency-converter/src/hooks/useCurrencyInfo.js
+++ currency-converter/src/hooks/useCurrencyInfo.js
@@ -1,5 +1,14 @@
 import { useEffect, useState } from "react";
 
-function useCurrencyInfo(){
-    
+function useCurrencyInfo(currency) {
+  const [data, setdata] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setdata(res[currency]));
+  }, [currency]);
+
+  return data;
 }
