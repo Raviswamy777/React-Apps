c4f1ba2cee7825078cb8b2c1fa2cf302ff6fc315
raviswamy777 <raviswamy@i.camp>
2024-08-14 23:49:34 +0530

    Currency app complete

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,18 +1,71 @@
 import { useState } from "react";
-import "./App.css";
-import {InputBox} from "./components/index.js";
+import useCurrencyInfo from "./hooks/useCurrencyInfo.js";
+import { InputBox } from "./components/index.js";
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
   return (
     <>
       <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
         style={{
           backgroundImage: `url("https://media.istockphoto.com/id/1327569515/photo/financial-investment-concept-stack-of-coins-for-finance-investor-with-trading-graph-growth.jpg?s=2048x2048&w=is&k=20&c=4i2cUB8QkRuRoDwUH0f0VhpGLfFOoB3p2t2MsrGW2kI=")`,
-        }}>
-          <h1>Coin Converter</h1>
+        }}
+      >
+        <h1>Coin Converter</h1>
+        <div className="w-full">
+          <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                convert();
+              }}
+            >
+              <div className="w-full mb-1">
+                <InputBox
+                  label="from"
+                  amount={amount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) => setFrom(currency)}
+                  onAmountChange={(amount) => setAmount(amount)}
+                  selectedCurency={from}
+                />
+              </div>
+              <div className="relative w-full h-0.5">
+                <button className="absolute left-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5" onClick={swap}>
+                  Swap
+                </button>
+                <InputBox
+                  label="to"
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) => setTo(currency)}
+                  onAmountChange={(amount) => setAmount(amount)}
+                  selectedCurency={to}
+                  amountDisabled
+                />
+              </div>
+              <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounde-lg">Convert {from.toUpperCase()} to {to.toUpperCase()}</button>
+            </form>
+          </div>
         </div>
-          <InputBox />
+      </div>
     </>
   );
 }
--- currency-converter/src/components/InputBox.jsx
+++ currency-converter/src/components/InputBox.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import {useId} from "react";
 
-function InputBox({
-  label,
+function InputBox(
+{  label,
   amount,
   onAmountChange,
   onCurrencyChange,
@@ -10,14 +10,17 @@ function InputBox({
   amountDisabled = false,
   currencyDisabled = false,
   className = "",
-}) {
+}
+) {
+  const id = useId();
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1-2">
-        <label htmlFor="" className="text-black/40 mb-2 inline-block">
+        <label htmlFor={id} className="text-black/40 mb-2 inline-block">
           {label}
         </label>
         <input
+        id={id}
           type="number"
           className="outline-none w-full bg-transparent py-1.5"
           placeholder="Amount"
